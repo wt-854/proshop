37f70cfd2bb2adae228bf469698df0792499193f
wt854 <wt854@outlook.com>
2020-10-08 15:19:05 +0800

    admin middleware & get users endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -105,4 +105,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile };
+// @desc    GET all users
+// @route   GET /api/users
+// @desc    Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
