67e0f5581357a8a461a37ad08c25bd5dc37c36c6
wt854 <wt854@outlook.com>
2020-10-09 17:55:02 +0800

    upload image frontend

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -10,7 +10,7 @@ const storage = multer.diskStorage({
   filename(req, file, cb) {
     cb(
       null,
-      `${ffile.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
     );
   },
 });
