1d67c4ffbd664f1653a97a3f0dc4433f344a52b7
wt854 <wt854@outlook.com>
2020-10-09 11:48:17 +0800

    delete product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  deleteProduct,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 // router.get('/', getProducts); OR
 router.route('/').get(getProducts);
 
 // router.get('/:id', getProductById); OR
-router.route('/:id').get(getProductById);
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
 
 export default router;
